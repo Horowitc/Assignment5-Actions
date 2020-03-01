38fe1bf7b7d1f39bb10245a6f76b9f8c880446d0
Caulin Horowitz <horowitc@oregonstate.edu>
2020-02-29 17:16:21 -0800

    Added area return function and test

--- task.py
+++ task.py
@@ -1,3 +1,11 @@
+import math
+
 
 def firstrun():
     return "success"
+
+def retarea(radius):
+    area = radius*radius*math.pi
+    area = round(area, 2)
+    return area
+
--- tests.py
+++ tests.py
@@ -12,6 +12,11 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def testarea(self):
+        area = task.retarea(2)
+        self.assertEqual(12.57, area)
+        area = task.retarea(4)
+        self.assertEqual(50.27, area)
 
 if __name__ == '__main__':
     unittest.main()
