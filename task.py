06d8aa7dc7efdca3c681cb546544017a159e5f0a
Caulin Horowitz <horowitc@oregonstate.edu>
2020-02-29 17:57:23 -0800

    Date Difference (Fixed Spacing)

--- task.py
+++ task.py
@@ -23,7 +23,7 @@ def leapyear(date):
 
 
 def retdate(date1, date2):
-    daysinmon = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,31]
+    daysinmon = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     num1 = date1[2]*365 + date1[1]
     for i in range(0, date1[0] - 1):
         num1 += daysinmon[i]
